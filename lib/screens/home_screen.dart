cc4c994c05adb9627edb71b0a8c7fd3fc52e4542
Deepak M S <souparnikad40@gmail.com>
2022-06-24 14:42:12 +0530

    screen issue fixed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -94,124 +94,126 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ],
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //display the user input
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 7,
-              ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                //put the text center left position
-                alignment: Alignment.centerLeft,
-                //display the text
-                child: Text(
-                  userQuestion,
-                  style:  TextStyle(
-                    fontSize: 30,
-                    color: themeNotifier.isDark ? Colors.white :CustomTheme.lightTheme.primaryColor,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //display the user input
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 8.3,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  //put the text center left position
+                  alignment: Alignment.centerLeft,
+                  //display the text
+                  child: Text(
+                    userQuestion,
+                    style:  TextStyle(
+                      fontSize: 30,
+                      color: themeNotifier.isDark ? Colors.white :CustomTheme.lightTheme.primaryColor,
+                    ),
                   ),
                 ),
-              ),
-              //display the answer
-              Container(
-                padding: const EdgeInsets.all(20),
-                //put the text center right
-                alignment: Alignment.centerRight,
-                //display the text
-                child: Text(
-                  userAnswer,
-                  style:  TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: themeNotifier.isDark ? Colors.white :CustomTheme.lightTheme.primaryColor,
+                //display the answer
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  //put the text center right
+                  alignment: Alignment.centerRight,
+                  //display the text
+                  child: Text(
+                    userAnswer,
+                    style:  TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: themeNotifier.isDark ? Colors.white :CustomTheme.lightTheme.primaryColor,
+                    ),
                   ),
                 ),
-              ),
-              //display the button-Grid view
-              Expanded(
-                child: GridView.builder(
-                  itemCount: buttons.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
+                //display the button-Grid view
+                Expanded(
+                  child: GridView.builder(
+                    itemCount: buttons.length,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      //clear button
+                      if (index == 0) {
+                        return MyButton(
+                          color: Colors.green,
+                          textColor: Colors.white,
+                          buttonText: buttons[index],
+                          buttonTapped: () {
+                            //clear userQuestions and userAnswer
+                            setState(() {
+                              userQuestion = '';
+                              userAnswer = '';
+                            });
+                          },
+                        );
+                      }
+                      //delete button
+                      if (index == 1) {
+                        return MyButton(
+                          color: Colors.red,
+                          textColor: Colors.white,
+                          buttonText: buttons[index],
+                          buttonTapped: () {
+                            //delete one index from userQuestion from the back
+                            setState(() {
+                              userQuestion = userQuestion.substring(
+                                  0, userQuestion.length - 1);
+                            });
+                          },
+                        );
+                      }
+                      //equal button and answer button
+                      if (index == buttons.length - 1) {
+                        return MyButton(
+                          color: themeNotifier.isDark ? CustomTheme.darkTheme.primaryColor :CustomTheme.lightTheme.primaryColor,
+                          textColor: Colors.white,
+                          buttonText: buttons[index],
+                          buttonTapped: () {
+                            setState(() {
+                              equalPressed();
+                            });
+                          },
+                        );
+                      }
+                      // answer button
+                      if (index == buttons.length - 2) {
+                        return MyButton(
+                          color: Colors.white,
+                          textColor: themeNotifier.isDark ? CustomTheme.darkTheme.primaryColor :CustomTheme.lightTheme.primaryColor,
+                          buttonText: buttons[index],
+                          buttonTapped: () {
+                            setState(() {
+                              equalPressed();
+                            });
+                          },
+                        );
+                      }
+                      return MyButton(
+                        color: isOperator(buttons[index])
+                            ? themeNotifier.isDark ? CustomTheme.darkTheme.primaryColor :CustomTheme.lightTheme.primaryColor
+                            : Colors.white,
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : themeNotifier.isDark ? CustomTheme.darkTheme.primaryColor :CustomTheme.lightTheme.primaryColor,
+                        buttonText: buttons[index],
+                        buttonTapped: () {
+                          //concatenate
+                          setState(() {
+                            userQuestion += buttons[index];
+                          });
+                        },
+                      );
+                    },
                   ),
-                  itemBuilder: (BuildContext context, int index) {
-                    //clear button
-                    if (index == 0) {
-                      return MyButton(
-                        color: Colors.green,
-                        textColor: Colors.white,
-                        buttonText: buttons[index],
-                        buttonTapped: () {
-                          //clear userQuestions and userAnswer
-                          setState(() {
-                            userQuestion = '';
-                            userAnswer = '';
-                          });
-                        },
-                      );
-                    }
-                    //delete button
-                    if (index == 1) {
-                      return MyButton(
-                        color: Colors.red,
-                        textColor: Colors.white,
-                        buttonText: buttons[index],
-                        buttonTapped: () {
-                          //delete one index from userQuestion from the back
-                          setState(() {
-                            userQuestion = userQuestion.substring(
-                                0, userQuestion.length - 1);
-                          });
-                        },
-                      );
-                    }
-                    //equal button and answer button
-                    if (index == buttons.length - 1) {
-                      return MyButton(
-                        color: themeNotifier.isDark ? CustomTheme.darkTheme.primaryColor :CustomTheme.lightTheme.primaryColor,
-                        textColor: Colors.white,
-                        buttonText: buttons[index],
-                        buttonTapped: () {
-                          setState(() {
-                            equalPressed();
-                          });
-                        },
-                      );
-                    }
-                    // answer button
-                    if (index == buttons.length - 2) {
-                      return MyButton(
-                        color: Colors.white,
-                        textColor: themeNotifier.isDark ? CustomTheme.darkTheme.primaryColor :CustomTheme.lightTheme.primaryColor,
-                        buttonText: buttons[index],
-                        buttonTapped: () {
-                          setState(() {
-                            equalPressed();
-                          });
-                        },
-                      );
-                    }
-                    return MyButton(
-                      color: isOperator(buttons[index])
-                          ? themeNotifier.isDark ? CustomTheme.darkTheme.primaryColor :CustomTheme.lightTheme.primaryColor
-                          : Colors.white,
-                      textColor: isOperator(buttons[index])
-                          ? Colors.white
-                          : themeNotifier.isDark ? CustomTheme.darkTheme.primaryColor :CustomTheme.lightTheme.primaryColor,
-                      buttonText: buttons[index],
-                      buttonTapped: () {
-                        //concatenate
-                        setState(() {
-                          userQuestion += buttons[index];
-                        });
-                      },
-                    );
-                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
